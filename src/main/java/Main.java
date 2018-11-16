cf4b959c46e0d6f220b01d00f6ed5bbc12ac9dc9
Javier Gonzalez <jgonzalez@dparadig.com>
2018-11-16 14:57:02 -0300

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+	private static final Logger log = LoggerFactory.getLogger(Main.class);
+	
+	public static void main(String[] args) {
+		log.info("info message");
+		log.debug("debugging");
+	}
+
+}
